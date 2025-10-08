5482703e7f9901edc32667ab2dbc81738f04ab3d
25LP4949 25LP4949 <25LP4949@G301-CC0194.local>
2025-10-08 12:39:56 +0800

    hehe

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,91 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import { nanoid } from "nanoid";
-
 const app: Application = express();
 const port = 4000;
-
-let tasks = [
-    { id: nanoid(), name: "Shalaa ugaah", mode: false },
-    { id: nanoid(), name: "Toosoo archih", mode: false },
-];
 app.use(cors());
 app.use(express.json());
-
+let categories = [
+    "All Dishes",
+    "Main Dishes",
+    "Salads",
+    "Appetizers",
+    "Pizzas",
+    "Lunch favorites",
+    "Fish & Sea foods",
+    "Brunch",
+    "Side dish ",
+    "Desserts",
+    "Beverages",
+];
+let foods = [
+    {
+        id: nanoid(),
+        name: "Food1",
+        price: 12345,
+        ingredients: "ingredients1"
+    },
+    {
+        id: nanoid(),
+        name: "Food2",
+        price: 54321,
+        ingredients: "ingredients2"
+    },
+]
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
-})
-app.get('/tasks', (req: Request, res: Response) => {
-    res.send(tasks)
-})
-app.post('/tasks', (req: Request, res: Response) => {
-    const id = nanoid()
+});
+app.post("/create-food", (req: Request, res: Response) => {
+    if (req.body === undefined) {
+        res.status(400).send("Error")
+    } else {
+        const { name } = req.body;
+        const { price } = req.body;
+        const { ingredients } = req.body;
+        foods.push(name, price, ingredients);
+    }
+});
+app.get("/categories", (req: Request, res: Response) => {
+    res.send(categories)
+});
+app.get("/foods", (req: Request, res: Response) => {
+    res.send(foods)
+});
+app.post("/categories", (req: Request, res: Response) => {
     const { name } = req.body;
-    const { mode } = req.body;
-    tasks.unshift({ id, name, mode });
-    res.status(201).send({ id });
-})
-app.delete('/tasks/:id', (req: Request, res: Response) => {
-    const id = req.params.id
-    const newTasks = tasks.filter((task) => task.id !== id)
-    tasks = newTasks;
-    res.send(tasks)
-})
-app.put('/tasks/:id', (req: Request, res: Response) => {
-    const id = req.params.id
-    const { name } = req.body;
-    const index = tasks.findIndex(task => task.id === id);
-    tasks[index].name = name;
-    res.send(tasks)
-})
-app.put('/toggle/tasks/:id', (req: Request, res: Response) => {
-    const id = req.params.id
-    const { mode } = req.body
-    const index = tasks.findIndex(task => task.id === id);
-    tasks[index].mode = mode;
-    res.send(tasks)
-})
+    categories.push(name);
+});
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+// app.get('/tasks', (req: Request, res: Response) => {
+//     res.send(tasks)
+// })
+// app.post('/tasks', (req: Request, res: Response) => {
+//     const id = nanoid()
+//     const { name } = req.body;
+//     const { mode } = req.body;
+//     tasks.unshift({ id, name, mode });
+//     res.status(201).send({ id });
+// })
+// app.delete('/tasks/:id', (req: Request, res: Response) => {
+//     const id = req.params.id
+//     const newTasks = tasks.filter((task) => task.id !== id)
+//     tasks = newTasks;
+//     res.send(tasks)
+// })
+// app.put('/tasks/:id', (req: Request, res: Response) => {
+//     const id = req.params.id
+//     const { name } = req.body;
+//     const index = tasks.findIndex(task => task.id === id);
+//     tasks[index].name = name;
+//     res.send(tasks)
+// })
+// app.put('/toggle/tasks/:id', (req: Request, res: Response) => {
+//     const id = req.params.id
+//     const { mode } = req.body
+//     const index = tasks.findIndex(task => task.id === id);
+//     tasks[index].mode = mode;
+//     res.send(tasks)
+// })
